bee82e887b56a9f773c1261cc07e86cf9917c76c
Valentin Bartschies <valentin.bartschies@tuhh.de>
2023-12-01 20:10:23 +0100

    task 2 almost finished

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::fs;
 
 fn get_lines(file: &str) -> Vec<String> {
     let content = fs::read_to_string(file).unwrap();
@@ -22,10 +22,63 @@ fn parse_line(line: &str) -> u32 {
     panic!("This shouldn't be reachable");
 }
 
-#[allow(unused)]
-fn parse_line_words(line: &str) -> u32 {
+fn parse_all_digits(line: &str) -> Vec<u32> {
+    let mut digits: Vec<u32> = vec![];
+    let mut i: usize = 0;
 
-    0
+    while i < line.len() {
+        let j = line.len() - i;
+        if let Some(d) = line.chars().nth(i).unwrap().to_digit(10) {
+            digits.push(d);
+            i += 1;
+            continue;
+        }
+        if j >= 5 {
+            if line[i..i+5].contains("three") {
+                digits.push(3);
+            }
+            if line[i..i+5].contains("seven") {
+                digits.push(7);
+            }
+            if line[i..i+5].contains("eight") {
+                digits.push(8);
+            }
+        }
+        if j >= 4 {
+            if line[i..i+4].contains("four") {
+                digits.push(4);
+            }
+            if line[i..i+4].contains("five") {
+                digits.push(6);
+            }
+            if line[i..i+4].contains("nine") {
+                digits.push(9);
+            }
+        }
+        if j >= 3 {
+            if line[i..i+3].contains("one") {
+                digits.push(1);
+            }
+            if line[i..i+3].contains("two") {
+                digits.push(2);
+            }
+            if line[i..i+3].contains("six") {
+                digits.push(6);
+            }
+        }
+        i += 1;
+    }
+    // todo go backwarts through string for finding the last digit
+    digits
+}
+
+
+
+fn parse_line_with_words(line: &str) -> u32 {
+    let digits = parse_all_digits(line);
+    let digit = digits.first().unwrap() * 10 + digits.last().unwrap();
+    // println!("{}", digit);
+    digit
 }
 
 fn task_1(file: &str) -> u32 {
@@ -44,9 +97,8 @@ fn task_2(file: &str) -> u32 {
     let lines = get_lines(file);
     let mut sum = 0;
     for line in lines {
-        let val = parse_line_words(&line);
+        let val = parse_line_with_words(&line);
         sum += val;
-        // println!("{val}");
     }
     sum
 
@@ -58,6 +110,8 @@ fn main() {
     );
 
     println!("task 2: {}",
-        task_2("./src/input_demo_2.txt")
-    )
+        task_2("./src/input.txt")
+    );
+
+    // println!("{:?}", parse_all_digits("twosssdfsftwone"));
 }
