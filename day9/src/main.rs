6ab7db7f256b27f5c388cf9deba8045d81226a51
Valentin Bartschies <valentin.bartschies@tuhh.de>
2023-12-11 00:08:51 +0100

    day 9 task 2 done

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -4,6 +4,8 @@ use eyre::Result;
 fn main() -> Result<()> {
     let task_1 = solve_task_1("input.txt")?;
     println!("task 1: {task_1}");
+    let task_2 = solve_task_2("input.txt")?;
+    println!("task 2: {task_2}");
     Ok(())
 }
 
@@ -20,6 +22,24 @@ fn solve_task_1(input: &str) -> Result<isize> {
     let guessed_numbers: Vec<isize> = input_numbers
         .iter()
         .map(|v| find_next_value(v.clone()))
+        .collect::<Vec<isize>>();
+
+    Ok(guessed_numbers.iter().sum::<isize>())
+}
+
+fn solve_task_2(input: &str) -> Result<isize> {
+    let input = fs::read_to_string(input)?;
+    let input_numbers = input
+        .lines()
+        .map(str::split_whitespace)
+        .map(|line| {
+            line.flat_map(str::parse::<isize>)
+                .collect::<Vec<isize>>()
+        })
+        .collect::<Vec<Vec<isize>>>();
+    let guessed_numbers: Vec<isize> = input_numbers
+        .iter()
+        .map(|v| find_prev_value(v.clone()))
         .collect::<Vec<isize>>();
 
     Ok(guessed_numbers.iter().sum::<isize>())
@@ -52,6 +72,32 @@ fn find_next_value(vec: Vec<isize>) -> isize {
     return *rows[0].last().unwrap();
 }
 
+fn find_prev_value(vec: Vec<isize>) -> isize {
+    let mut rows: Vec<Vec<isize>> = Vec::new();
+    rows.push(vec.clone());
+    for i in 0..vec.len() {
+        let mut row: Vec<isize> = Vec::new();
+        for j in 0..(rows[i].len() - 1) {
+            let diff = (rows[i][j+1] - rows[i][j]) as isize;
+            row.push(diff);
+        }
+        if row.iter().all(|n| *n == 0) {
+            row.push(0);
+            rows.push(row);
+            break;
+        }
+        rows.push(row);
+    }
+
+    for i in (1..rows.len()).rev() {
+        let diff: isize = *rows[i].first().unwrap();
+        let first: isize = *rows[i-1].first().unwrap();
+        rows[i-1].insert(0, first - diff);
+    }
+
+    return *rows[0].first().unwrap();
+}
+
 
 #[test]
 fn test_task_1_demo() -> Result<()> {
@@ -64,5 +110,19 @@ fn test_task_1_demo() -> Result<()> {
 fn test_task_1_real() -> Result<()> {
     let result = solve_task_1("input.txt")?;
     assert_eq!(result, 2174807968);
+    Ok(())
+}
+
+#[test]
+fn test_task_2_demo() -> Result<()> {
+    let result = solve_task_2("input_demo.txt")?;
+    assert_eq!(result, 2);
+    Ok(())
+}
+
+#[test]
+fn test_task_2_real() -> Result<()> {
+    let result = solve_task_2("input.txt")?;
+    assert_eq!(result, 1208);
     Ok(())
 }
